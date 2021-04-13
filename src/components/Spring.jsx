a8125d7a5916b1f9aea4613921767c79bf132663
hkpark130 <hkpark130@naver.com>
2021-04-14 05:13:13 +0900

    https://hkpark130.p-e.kr:8080/issues/12

--- src/components/Spring.jsx
+++ src/components/Spring.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import imgSpring from '../images/spring_boot.png';
+import imgSpring from '../images/spring-dia.png';
 
 function Spring() {
     return(
         <div class="component">
             <h1>Spring</h1>
-
-            <div>
+                Spring Boot를 이용하여 웹을 만들어보았습니다.<br/>
+                사용했던 기술들을 정리하고 공유하기 위한 블로그를 기획하였습니다.
                 <div>
                     <pre>
                         <ul>
@@ -25,16 +25,27 @@ function Spring() {
                         </ul> 
                     </pre>
                 </div>
+
+                <h1>프로젝트 링크</h1>
+                <a href='https://hkpark130.p-e.kr:8100/'>
+                    <h2>스프링 블로그 서버로 이동</h2>
+                </a>
+
+                <h1>서버 구성도</h1>
                 <img
                     src={ imgSpring }
-                    width='256'
-                    height='128'
+                    width='800'
+                    height='431'
                 />
-            </div>
 
-            <a href='https://hkpark130.p-e.kr:8100/'>
-                <h2>서버로 이동</h2>
-            </a>
+            <div>
+                <h1>해당 프로젝트 Github 주소</h1>
+                인트로 페이지 소스코드는&nbsp;
+                <a href='https://github.com/hkpark130/Spring-Blog'>
+                    <u>https://github.com/hkpark130/Spring-Blog</u>
+                </a>
+                를 확인해주세요.<br/><br/>
+            </div>
         </div>
         
     );
