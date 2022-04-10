3d810df36995fb53297459a154080437506a60b1
hyeonhyeong.park <hyeonhyeong.park@hyeonkyeongparknoMacBook-Pro.local>
2022-04-10 23:03:10 +0900

    etc : deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sudo docker-compose -f /home/ec2-user/build/production.yml up -d --build
+APPLICATION_NAME=react-intro
+
+CONTAINER_ID=$(docker ps | grep react-intro | awk '{print $1}')
+
+if [ $CONTAINER_ID ]
+then
+  docker stop $CONTAINER_ID
+  docker rm $CONTAINER_ID
+fi
+
+cd /home/ec2-user/build
+docker-compose -f /home/ec2-user/build/production.yml up -d --build
